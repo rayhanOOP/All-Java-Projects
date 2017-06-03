fec462d34cecfd4caef7734e61078894ea260ee0
Mohammad Rayhanur Rahman <mirayhancse007@gmail.com>
2017-06-03 22:20:16 +0200

    Add files via upload

---
+++ 018.GradesAndPoints/src/GradesAndPoints.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class GradesAndPoints {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Type the points [0-60]: ");
+        int points = Integer.parseInt(reader.nextLine());
+        
+        if (points >=30 && points <35){
+            System.out.println("Grade: 1");
+    }
+     else if (points >=35 && points <40){
+            System.out.println("Grade: 2");
+    }
+     else if (points >=40 && points <45){
+            System.out.println("Grade: 3");
+    }
+     else if (points >=45 && points <50){
+            System.out.println("Grade: 4");
+    }
+     else if (points >= 50){
+            System.out.println("Grade: 5");
+    }
+     else 
+            System.out.println("Grade: failed");
+    }
+}
