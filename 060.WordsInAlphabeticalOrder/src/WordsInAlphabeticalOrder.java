dcd732912619a04a3ae2fd525be7b4baa0f86d3a
Mohammad Rayhanur Rahman <mirayhancse007@gmail.com>
2017-06-05 12:56:28 +0200

    Add files via upload

---
+++ 060.WordsInAlphabeticalOrder/src/WordsInAlphabeticalOrder.java
@@ -0,0 +1,36 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class WordsInAlphabeticalOrder {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        // create here an ArrayList
+        ArrayList<String> words = new ArrayList<String>();
+           
+        
+        while (true){
+            
+            System.out.println("Type a word: ");
+            String word = reader.nextLine();
+            
+            if (word.isEmpty()){
+                System.out.println("You typed the following words: " );
+                break;
+            }
+            
+                words.add(word);
+                
+        }
+        
+        Collections.sort(words);
+        
+        for (String w : words){
+            
+            System.out.println(w);
+        }
+        
+    }
+}
